01263b2f2c16478e8833d3ef126471fe77783559
Oskaroo <oskarmno@gmail.com>
2023-05-28 21:14:29 +0200

    Zadanie 3

---
+++ Zadanie 3/Zadanie 3/Zadanie 3/MultifunctionalDevice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ver1.IDevice;
+using ver1;
+
+namespace Zadanie_3;
+
+public class MultifunctionalDevice : Copier, IFax
+{
+    public void Fax(IDocument document, string recipientNumber)
+    {
+        if (GetState() == State.on)
+        {
+            Console.WriteLine($"{DateTime.Now} Fax: {document.GetFileName()} to {recipientNumber}");
+        }
+    }
+}
---
+++ Zadanie 3/Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Kserokopiarka;
+using ver1;
+
+class Program
+{
+    static void Main()
+        //sprawdzenie zadania 1 
+    {
+        var xerox = new Copier();
+        xerox.PowerOn();
+        IDocument doc1 = new PDFDocument("aaa.pdf");
+        xerox.Print(in doc1);
+
+        IDocument doc2;
+        xerox.Scan(out doc2);
+
+        xerox.ScanAndPrint();
+        System.Console.WriteLine(xerox.Counter);
+        System.Console.WriteLine(xerox.PrintCounter);
+        System.Console.WriteLine(xerox.ScanCounter);
+
+        //sprawdzenie zadania 2 
+        MultifunctionalDevice mfd = new MultifunctionalDevice();
+        mfd.PowerOn();
+
+        IDocument document = new PDFDocument("document.pdf");
+        mfd.Print(document);
+
+        IDocument scannedDocument;
+        mfd.Scan(out scannedDocument);
+
+        mfd.ScanAndPrint();
+
+        string recipientNumber = "123456789";
+        mfd.Fax(document, recipientNumber);
+
+        mfd.PowerOff();
+    }
+}
